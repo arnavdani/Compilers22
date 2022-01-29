ca20d619d416b55e1b95783fabad4102a4d763ea
Arnav Dani <60204322+arnavdani@users.noreply.github.com>
2022-01-28 16:00:44 -0800

    Add files via upload

--- scanner/Scanner.java
+++ scanner/Scanner.java
@@ -5,6 +5,7 @@ import java.io.*;
 /**
  * Scanner is a simple scanner for Compilers and Interpreters (2014-2015) lab exercise 1
  * @author Arnav Dani
+ * @version 1-28-22
  *  
  * A basic simplified scanner that can read letters, numbers, and elemtary operators
  *
@@ -107,7 +108,7 @@ public class Scanner
      */
     public static boolean isLetter(char s)
     {
-    	return (s >= 'a' &&  s <= 'z') || (s >= 'A' || s <= 'Z');
+    	return (s >= 'a' &&  s <= 'z') || (s >= 'A' && s <= 'Z');
     }
     
     /**
@@ -122,13 +123,49 @@ public class Scanner
     
     /**
      * checks if the inputted character is an operator
+     * 
      * @param s the character being evaluated
      * @return true if it is an operator; otherwise, false
      */
     public static boolean isOperator(char s)
     {
         return s == '=' || s == '+' || s == '-' || s == '*' || s == '/' || s == '%' 
-            || s == '(' || s == ')';
+            || s == '(' || s == ')' || s == ':' || s == '<' || s == '>';
+    }
+    
+    /**
+     * checks if the inputted operator is an operator
+     * that can be a 2 character operator
+     * 
+     * :=, <=, >=, ==, <>
+     * @param s
+     * @return
+     */
+    public static boolean isDuplOperator(char s)
+    {
+    	return s == '=' || s == ':' || s == '<' || s == '>';
+    }
+    
+    /**
+     * checks if at the end of the file
+     * by checking if current char is a period
+     * @param s the character being evaluated
+     * @return true if it is a period; otherwise false
+     */
+    public static boolean isAtEndofFile(char s)
+    {
+    	return s == '.';
+    }
+    
+    /**
+     * checks if at the end of a statement
+     * currently a semicolon signifies the end of a statement
+     * @param s the character being evaluated
+     * @return true if s is a semicolon; otherwise, false
+     */
+    public static boolean isAtEndOfLine(char s)
+    {
+    	return s == ';';
     }
     
     /**
@@ -141,7 +178,7 @@ public class Scanner
     {
     	//test for first number
     	String num = "";
-    	if (isDigit(currentChar))
+    	if (isDigit(currentChar) && !isWhiteSpace(currentChar))
     	{
     		num += currentChar;   	
     		if (hasNext())
@@ -153,7 +190,7 @@ public class Scanner
     		throw new ScanErrorException("Can't find a number");
     	
     	//read the rest of the digits
-    	while (isDigit(currentChar))
+    	while (isDigit(currentChar) && !isWhiteSpace(currentChar))
     	{
     		num += currentChar;
     		if (hasNext())
@@ -186,7 +223,7 @@ public class Scanner
     		throw new ScanErrorException("Not valid identifier - must start with letter");
     	
     	//rest of the identifier
-    	while (isLetter(currentChar))
+    	while ((isLetter(currentChar) || isDigit(currentChar)) && !isWhiteSpace(currentChar))
     	{
     		id += currentChar;
     		if (hasNext())
@@ -219,7 +256,7 @@ public class Scanner
     		throw new ScanErrorException("Not valid operator");
     	
     	//rest of the operator
-    	while (isOperator(currentChar))
+    	while (isDuplOperator(currentChar))
     	{
     		op += currentChar;
     		if (hasNext())							
@@ -239,21 +276,56 @@ public class Scanner
     	String token = "";
     	if(hasNext())
     	{
-    		if (isLetter(currentChar))
-    			token = scanIdentifier();
-    		else if(isDigit(currentChar))
-    			token = scanNumber();
-    		else if(isOperator(currentChar))
-    			token = scanOperator();
-    		else if(isWhiteSpace(currentChar))
+    		while (isWhiteSpace(currentChar))
     		{
+    			eat(currentChar);		
+    		}		
+    		
+    		if (isLetter(currentChar))
+    		{
+    			token = scanIdentifier();
     			return token;
+    			//System.out.println(token);
     		}
-    		else
-    			throw new ScanErrorException("No characters recogized");
+    			
+    		else if(isDigit(currentChar))
+    		{
+    			token = scanNumber();
+    			return token;
+    			//System.out.println(token);
+    		}
+    			
+    		else if(isOperator(currentChar))
+    		{
+    			token = scanOperator();
+    			return token;
+    			//System.out.println(token);
+    		}
+    		
+    		else if(isAtEndofFile(currentChar))
+    		{
+    			token = "END";
+    			eof = true;
+    		}
+    		
+    		else if(isAtEndOfLine(currentChar))
+    		{
+    			token = "EOL";
+    			eat(currentChar);
+    		}
+    		
     	}
     	
-    	
-    	return token;
+    	if (token != "")
+    	{
+    		return token;
+    	}
+    	else
+    	{
+    		System.out.println("errchar:" + currentChar);
+    		throw new ScanErrorException(currentChar + " not recogized"); 		
+    		
+    	}
+			
     }    
 }
